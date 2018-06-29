0d1f1ec531e68e14e2268cef46a158bf626a14e5
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-06-29 17:02:00 +0300

    Performance tests for FilterDigits task were added.

---
+++ FilterDigits/Digits.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDigits
+{
+    public class Digits
+    {
+        /// <summary>
+        /// Filters passed array. Returns array that consists only 
+        /// from elements that contain passed digit.
+        /// </summary>
+        /// <param name="array">
+        /// Array that needs to be filtered.
+        /// </param>
+        /// <param name="digit">
+        /// Digit that should be in result array's elements.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int[]"/>.
+        /// Filtered array.
+        /// </returns>
+        public static int[] FilterDigitsWithStr(int[] array, int digit)
+        {
+            int swapIndex = 0;
+            string strDigit = digit.ToString();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i].ToString().Contains(strDigit))
+                {
+                    Swap(ref array[swapIndex], ref array[i]);
+                    swapIndex++;
+                }
+            }
+
+            Array.Resize(ref array, swapIndex);
+
+            return array;
+        }
+
+        /// <summary>
+        /// Filters passed array. Returns array that consists only 
+        /// from elements that contain passed digit.
+        /// </summary>
+        /// <param name="array">
+        /// Array that needs to be filtered.
+        /// </param>
+        /// <param name="digit">
+        /// Digit that should be in result array's elements.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int[]"/>.
+        /// Filtered array.
+        /// </returns>
+        public static int[] FilterDigits(int[] array, int digit)
+        {
+            int swapIndex = 0;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (!ContainsDigit(array[i], digit))
+                {
+                    continue;
+                }
+
+                Swap(ref array[swapIndex], ref array[i]);
+                swapIndex++;
+            }
+
+            Array.Resize(ref array, swapIndex);
+
+            return array;
+        }
+
+        /// <summary>
+        /// Checks if passed number contains passed digit.
+        /// </summary>
+        /// <param name="number">
+        /// Number that needs to be checked.
+        /// </param>
+        /// <param name="digit">
+        /// Digit that may be in the number.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// True if number contains digit, false otherwise.
+        /// </returns>
+        private static bool ContainsDigit(int number, int digit)
+        {
+            while (number > 0)
+            {
+                if (number % 10 == digit)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Swaps values of two passed elements.
+        /// </summary>
+        /// <param name="a">
+        /// First element.
+        /// </param>
+        /// <param name="b">
+        /// Second element.
+        /// </param>
+        private static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+    }
+}
---
+++ PerformanceTestForFilterDigit/DigitsPerformanceTest.cs
@@ -0,0 +1,70 @@
+﻿#define DEBUG
+
+namespace PerformanceTestForFilterDigit
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using FilterDigits;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DigitsPerformanceTest
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var listener = new DelimitedListTraceListener(@"PerformanceOutput.log");
+            Debug.Listeners.Add(listener);
+        }
+
+        [Test]
+        public void FilterDigitNumber_PerformanceTest()
+        {
+            var rng = new Random(0);
+            int[] array = Enumerable.Repeat(0, 10000000)
+                                    .Select(v => rng.Next())
+                                    .ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int digit = 0; digit < 10; digit++)
+            {
+                Digits.FilterDigits(array, digit);
+            }
+
+            sw.Stop();
+
+            SaveResultToLog(sw.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void FilterDigitString_PerformanceTest()
+        {
+            var rng = new Random(0);
+            int[] array = Enumerable.Repeat(0, 10000000)
+                                    .Select(v => rng.Next())
+                                    .ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int digit = 0; digit < 10; digit++)
+            {
+                Digits.FilterDigits(array, digit);
+            }
+
+            sw.Stop();
+
+            SaveResultToLog(sw.ElapsedMilliseconds);
+        }
+
+        private static void SaveResultToLog(long time)
+        {
+            Debug.WriteLine($"Filtering digits 10 times with ints in {time} ms.");
+            Debug.Flush();
+        }
+    }
+}
