63fdf6eca51652915fd2c4e2f89a904882f94639
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-03 18:46:21 +0300

    Tests for Binary GCD were added.

--- GCD.Tests/GcdTests.cs
+++ GCD.Tests/GcdTests.cs
@@ -15,6 +15,14 @@
         [TestCase(1071, 462, ExpectedResult = 21)]
         public int Euclid_TwoNonZeroNumbersInput_ValidResult(int a, int b) => Gcd.Euclid(a, b);
 
+        [TestCase(54, 24, ExpectedResult = 6)]
+        [TestCase(54, -24, ExpectedResult = 6)]
+        [TestCase(42, 56, ExpectedResult = 14)]
+        [TestCase(252, 105, ExpectedResult = 21)]
+        [TestCase(-252, -105, ExpectedResult = 21)]
+        [TestCase(1071, 462, ExpectedResult = 21)]
+        public int Binary_TwoNonZeroNumbersInput_ValidResult(int a, int b) => Gcd.Binary(a, b);
+
         [TestCase(18, 24, 36, ExpectedResult = 6)]
         [TestCase(15, 35, 75, ExpectedResult = 5)]
         [TestCase(-15, 35, -75, ExpectedResult = 5)]
@@ -24,6 +32,16 @@
         [TestCase(423748, 42398, 326714, 21170, ExpectedResult = 58)]
         public int Euclid_MultipleNonZeroNumbersInput_ValidResult(int a, int b, params int[] numbers) => 
             Gcd.Euclid(a, b, numbers);
+
+        [TestCase(18, 24, 36, ExpectedResult = 6)]
+        [TestCase(15, 35, 75, ExpectedResult = 5)]
+        [TestCase(-15, 35, -75, ExpectedResult = 5)]
+        [TestCase(8, 16, 76, 12, ExpectedResult = 4)]
+        [TestCase(-8, -16, -76, -12, ExpectedResult = 4)]
+        [TestCase(1080, 3768, 5812, 12222, ExpectedResult = 2)]
+        [TestCase(423748, 42398, 326714, 21170, ExpectedResult = 58)]
+        public int Binary_MultipleNonZeroNumbersInput_ValidResult(int a, int b, params int[] numbers) =>
+            Gcd.Binary(a, b, numbers);
 
         [TestCase(0, 0)]
         [TestCase(50, 110, 20, 0, 30, 0)]
